dd84cc67429145f9a69ac03f4bec775900b6b8f3
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2016-07-14 22:19:42 +0300

    Add configurable hello world HTTP server

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           System.Environment (getEnv)
+import qualified Network.Wai as Wai (responseLBS, Application)
+import qualified Network.Wai.Handler.Warp as Warp (run)
+import           Network.HTTP.Types (status200)
+
+app :: Wai.Application
+app req respond =
+    respond $ Wai.responseLBS status200 [] "Hello World"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    token <- getEnv "AKAHSA_SLACK_TOKEN"
+    url <- getEnv "AKAHSA_SLACK_URL"
+    portString <- getEnv "AKAHSA_PORT"
+    let port = read portString :: Int
+    putStrLn ("token: \"" ++ token ++ "\", url: \"" ++ url ++ "\"")
+    Warp.run port app
