12c862274c98b736b9485b4ee8fb72f287429d29
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2016-09-04 14:35:53 +0300

    Add /pains command
    Will be used for listing all current pains.

--- src/lib/Akahsa/Commands.hs
+++ src/lib/Akahsa/Commands.hs
@@ -3,6 +3,7 @@
 
 module Akahsa.Commands (
     stripOuchCommand,
+    stripPainsCommand,
     sayHi
 ) where
 
@@ -11,20 +12,26 @@ import qualified Data.Text as T (Text, pack, unpack, append)
 import qualified Web.Slack as S (SlackBot, Event(Message))
 import qualified Web.Slack.Message as M (sendMessage)
 
-stripOuchCommand' :: String -> Maybe String
-stripOuchCommand' text =
-    let regex = "^/?[Oo]uch[,:.]?[[:space:]]*" :: String
-        prefixMatch = text =~ regex :: (String,String,String)
+type Regex = String
+
+stripRegex :: Regex -> String -> Maybe String
+stripRegex regex text =
+    let prefixMatch = text =~ regex :: (String,String,String)
      in case prefixMatch of
           (_,"",_)   -> Nothing
           (_,_,rest) -> Just rest
 
 stripOuchCommand :: T.Text -> Maybe T.Text
-stripOuchCommand = fmap T.pack . stripOuchCommand' . T.unpack
+stripOuchCommand = fmap T.pack . stripRegex "^/?[Oo]uch[,:.]?[[:space:]]*" . T.unpack
+
+stripPainsCommand :: T.Text -> Maybe T.Text
+stripPainsCommand = fmap T.pack . stripRegex "^/?[Pp]ains[,:.]?[[:space:]]*" . T.unpack
 
 sayHi :: S.SlackBot ()
 sayHi (S.Message cid _ (stripOuchCommand -> Just pain) _ _ _) =
     M.sendMessage cid ("So you're hurting? " `T.append` pain)
+sayHi (S.Message cid _ (stripPainsCommand -> Just _) _ _ _) =
+    M.sendMessage cid "Here are the pains: ..."
 sayHi (S.Message cid _ _ _ _ _) = M.sendMessage cid "Hello, World!"
 sayHi _ = return ()
 
--- tests/test.hs
+++ tests/test.hs
@@ -1,14 +1,36 @@
 import           Test.Tasty
 import           Test.Tasty.Hspec
 import qualified Data.Text as T (pack)
-import qualified Akahsa.Commands as Com (stripOuchCommand)
+import qualified Akahsa.Commands as Com (stripOuchCommand, stripPainsCommand)
 
 main :: IO ()
 main = unitTests >>= defaultMain
 
 unitTests :: IO TestTree
-unitTests = testSpec "Unit tests" $
-    describe "stripOuchCommand" $
+unitTests = testSpec "Unit tests" $ do
+    describe "stripOuchCommand" $ do
         it "should strip lower case 'ouch'" $
             Com.stripOuchCommand (T.pack "ouch asdf") `shouldBe` Just (T.pack "asdf")
+
+        it "should strip capitalized 'ouch'" $
+            Com.stripOuchCommand (T.pack "Ouch asdf") `shouldBe` Just (T.pack "asdf")
+
+        it "should strip slash prefixed 'ouch'" $
+            Com.stripOuchCommand (T.pack "/ouch asdf") `shouldBe` Just (T.pack "asdf")
+
+        it "should strip slash prefixed capitalized 'ouch'" $
+            Com.stripOuchCommand (T.pack "/Ouch asdf") `shouldBe` Just (T.pack "asdf")
+
+    describe "stripPainsCommand" $ do
+        it "should strip lower case 'pains'" $
+            Com.stripPainsCommand (T.pack "pains asdf") `shouldBe` Just (T.pack "asdf")
+
+        it "should strip capitalized 'pains'" $
+            Com.stripPainsCommand (T.pack "Pains asdf") `shouldBe` Just (T.pack "asdf")
+
+        it "should strip slash prefixed 'pains'" $
+            Com.stripPainsCommand (T.pack "/pains asdf") `shouldBe` Just (T.pack "asdf")
+
+        it "should strip slash prefixed capitalized 'pains'" $
+            Com.stripPainsCommand (T.pack "/Pains asdf") `shouldBe` Just (T.pack "asdf")
 
