5cebe13c9777d2464ad3fa04daf23c6a7d769be2
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-08-26 22:15:32 +0200

    Compiles with DB pool

--- src/lib/Akahsa/Commands.hs
+++ src/lib/Akahsa/Commands.hs
@@ -7,10 +7,13 @@ module Akahsa.Commands (
     sayHi
 ) where
 
+-- TODO Parser instead?
 import           Text.Regex.TDFA ((=~))
 import qualified Data.Text as T (Text, pack, unpack, append)
 import qualified Web.Slack as S (SlackBot, Event(Message))
 import qualified Web.Slack.Message as M (sendMessage)
+import Database.Persist.Sql (SqlBackend)
+import Data.Pool (Pool)
 
 type Regex = String
 
@@ -27,11 +30,11 @@ stripOuchCommand = fmap T.pack . stripRegex "^/?[Oo]uch[,:.]?[[:space:]]*" . T.u
 stripPainsCommand :: T.Text -> Maybe T.Text
 stripPainsCommand = fmap T.pack . stripRegex "^/?[Pp]ains[,:.]?[[:space:]]*" . T.unpack
 
-sayHi :: S.SlackBot ()
-sayHi (S.Message cid _ (stripOuchCommand -> Just pain) _ _ _) =
+sayHi :: Pool SqlBackend -> S.SlackBot ()
+sayHi _ (S.Message cid _ (stripOuchCommand -> Just pain) _ _ _) =
     M.sendMessage cid ("So you're hurting? " `T.append` pain)
-sayHi (S.Message cid _ (stripPainsCommand -> Just _) _ _ _) =
+sayHi _ (S.Message cid _ (stripPainsCommand -> Just _) _ _ _) =
     M.sendMessage cid "Here are the pains: ..."
-sayHi (S.Message cid _ _ _ _ _) = M.sendMessage cid "Hello, World!"
-sayHi _ = return ()
+sayHi _ (S.Message cid _ _ _ _ _) = M.sendMessage cid "Hello, World!"
+sayHi _ _ = return ()
 
--- src/lib/Akahsa/Main.hs
+++ src/lib/Akahsa/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- TODO update resolver in stack.yaml + remove transformers from akahsa.cabal
 module Akahsa.Main (
     runPainBot
 ) where
@@ -8,10 +9,14 @@ import System.Environment (getEnv)
 import Web.Slack (runBot)
 import Web.Slack.Config (SlackConfig(..))
 import Akahsa.Commands (sayHi)
+import Database.Persist.Postgresql (withPostgresqlPool)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Logger (runNoLoggingT)
 
 runPainBot :: IO ()
 runPainBot = do
     token <- getEnv "AKAHSA_SLACK_TOKEN"
     let conf = SlackConfig { _slackApiToken = token }
-    runBot conf sayHi ()
-
+    let connectionString = "host=localhost port=5432 user=salemove dbname=akahsa password=salemovepass"
+    runNoLoggingT $ withPostgresqlPool connectionString 2 (\pool ->
+        liftIO $ runBot conf (sayHi pool) ())
